b9dd3db79484d81315eec12f52fda8b9495270d1
Dario MF <ferrada_86@hotmail.com>
2021-04-25 18:44:34 +0200

    first commit app-MyBlog

---
+++ src/AppMyBlog.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+
+const AppMyBlog = () => {
+
+    return (
+        <div>
+            <h1>App MyBlog</h1>
+        </div>
+    );
+};
+
+export default AppMyBlog;
